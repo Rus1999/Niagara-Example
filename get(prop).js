28f83a171d20b5ef5626d71509c61060ebb1925c
Rus@Progtech <punyarit.kla.progtech@gmail.com>
2023-11-09 16:06:05 +0700

    add get(prop).js

---
+++ get(prop).js
@@ -0,0 +1,4 @@
+// return a property's value
+// when instance of a complex is created, auto-generated accessosr ar ecreate to make accessing a frozen propty's value convenient
+
+// performance notes:
